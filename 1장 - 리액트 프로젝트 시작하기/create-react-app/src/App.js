4586b56dccf3ab383d87c22c01d04e5254f08d32
heo-it <yejin097@naver.com>
2023-01-24 18:02:56 +0900

    feat: create-react-app 사용하기

---
+++ 1장 - 리액트 프로젝트 시작하기/create-react-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function onClick() {
+    import('./data.json').then((data) => {
+      console.log(data);
+    });
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <button onClick={onClick}>클릭해보셔유 :)</button>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
