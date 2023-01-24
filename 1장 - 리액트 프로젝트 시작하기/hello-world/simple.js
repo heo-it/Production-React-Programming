2d7f729cbdd45e095cfcdfe1e58fb0ff60316507
heo-it <yejin097@naver.com>
2023-01-24 15:35:40 +0900

    feat: babel 사용하기

---
+++ 1장 - 리액트 프로젝트 시작하기/hello-world/simple.js
@@ -0,0 +1,23 @@
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? '좋아요 취소' : '좋아요';
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text);
+}
+;
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", {
+    style: {
+      marginTop: 20
+    }
+  }, /*#__PURE__*/React.createElement("p", null, `현재 카운트 : ${count}`), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
+}
+;
+const domContainer = document.getElementById('root');
+ReactDOM.render( /*#__PURE__*/React.createElement(Container, null), domContainer);
